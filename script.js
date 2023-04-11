ce77713c36b0cac27e0a97be4f15e963a1a0e023
PRIYA BHATT <41810819+ipriyabhatt@users.noreply.github.com>
2023-04-12 00:00:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+// script.js
+document.getElementById('add-btn').addEventListener('click', function() {
+  const todoInput = document.getElementById('todo-input');
+  const todoText = todoInput.value.trim();
+
+  if (todoText) {
+    const todoList = document.getElementById('todo-list');
+    const listItem = createTaskListItem(todoText);
+    todoList.appendChild(listItem);
+    todoInput.value = '';
+  }
+});
+
+function createTaskListItem(taskText) {
+  const listItem = document.createElement('li');
+  
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.addEventListener('change', function() {
+    listItem.style.textDecoration = checkbox.checked ? 'line-through' : 'none';
+  });
+
+  const label = document.createElement('label');
+  label.textContent = taskText;
+
+  const deleteIcon = document.createElement('i');
+  deleteIcon.className = 'material-icons';
+  deleteIcon.textContent = 'delete';
+  deleteIcon.addEventListener('click', function() {
+    listItem.remove();
+  });
+
+  listItem.appendChild(checkbox);
+  listItem.appendChild(label);
+  listItem.appendChild(deleteIcon);
+
+  return listItem;
+}
